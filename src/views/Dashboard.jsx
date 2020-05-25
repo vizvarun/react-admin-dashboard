e1d5bdb6f34c117369b4dd305df56c06ed93cfce
Varun Vishal <varunvishal10@gmail.com>
2020-05-25 11:49:25 +0530

    main panel white

--- src/views/Dashboard.jsx
+++ src/views/Dashboard.jsx
@@ -18,7 +18,7 @@ import {
   dashboardNASDAQChart,
 } from "variables/charts.jsx";
 
-import './main.css';
+import "./main.css";
 
 class Dashboard extends React.Component {
   render() {
@@ -26,32 +26,6 @@ class Dashboard extends React.Component {
       <>
         <div className="content">
           <Row>
-            <Col md="4">
-              <Card>
-                <CardHeader>
-                  <CardTitle tag="h5">Email Statistics</CardTitle>
-                  <p className="card-category">Last Campaign Performance</p>
-                </CardHeader>
-                <CardBody>
-                  <Pie
-                    data={dashboardEmailStatisticsChart.data}
-                    options={dashboardEmailStatisticsChart.options}
-                  />
-                </CardBody>
-                <CardFooter>
-                  <div className="legend">
-                    <i className="fa fa-circle text-primary" /> Opened{" "}
-                    <i className="fa fa-circle text-warning" /> Read{" "}
-                    <i className="fa fa-circle text-danger" /> Deleted{" "}
-                    <i className="fa fa-circle text-gray" /> Unopened
-                  </div>
-                  <hr />
-                  <div className="stats">
-                    <i className="fa fa-calendar" /> Number of emails sent
-                  </div>
-                </CardFooter>
-              </Card>
-            </Col>
             <Col md="8">
               <Card className="card-chart">
                 <CardHeader>
@@ -74,6 +48,32 @@ class Dashboard extends React.Component {
                   <hr />
                   <div className="card-stats">
                     <i className="fa fa-check" /> Data information certified
+                  </div>
+                </CardFooter>
+              </Card>
+            </Col>
+            <Col md="4">
+              <Card>
+                <CardHeader>
+                  <CardTitle tag="h5">Email Statistics</CardTitle>
+                  <p className="card-category">Last Campaign Performance</p>
+                </CardHeader>
+                <CardBody>
+                  <Pie
+                    data={dashboardEmailStatisticsChart.data}
+                    options={dashboardEmailStatisticsChart.options}
+                  />
+                </CardBody>
+                <CardFooter>
+                  <div className="legend">
+                    <i className="fa fa-circle text-primary" /> Opened{" "}
+                    <i className="fa fa-circle text-warning" /> Read{" "}
+                    <i className="fa fa-circle text-danger" /> Deleted{" "}
+                    <i className="fa fa-circle text-gray" /> Unopened
+                  </div>
+                  <hr />
+                  <div className="stats">
+                    <i className="fa fa-calendar" /> Number of emails sent
                   </div>
                 </CardFooter>
               </Card>
