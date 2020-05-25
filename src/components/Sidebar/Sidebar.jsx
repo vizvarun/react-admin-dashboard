b3a9dae8476e1ba41bb604156cb52beeed8d413c
Varun Vishal <varunvishal10@gmail.com>
2020-05-25 10:12:59 +0530

    1st init

---
+++ src/components/Sidebar/Sidebar.jsx
@@ -0,0 +1,69 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+import { Nav } from "reactstrap";
+import PerfectScrollbar from "perfect-scrollbar";
+import './Sidebar.css';
+
+import logo from "logo.svg";
+
+var ps;
+
+class Sidebar extends React.Component {
+  constructor(props) {
+    super(props);
+    this.activeRoute.bind(this);
+    this.sidebar = React.createRef();
+  }
+  activeRoute(routeName) {
+    return this.props.location.pathname.indexOf(routeName) > -1 ? "active" : "";
+  }
+  componentDidMount() {
+    if (navigator.platform.indexOf("Win") > -1) {
+      ps = new PerfectScrollbar(this.sidebar.current, {
+        suppressScrollX: true,
+        suppressScrollY: false,
+      });
+    }
+  }
+  componentWillUnmount() {
+    if (navigator.platform.indexOf("Win") > -1) {
+      ps.destroy();
+    }
+  }
+  render() {
+    return (
+      <div
+        className="sidebar"
+        data-color={this.props.bgColor}
+        data-active-color={this.props.activeColor}
+      >
+        <div className="sidebar-wrapper" ref={this.sidebar}>
+          <Nav>
+            {this.props.routes.map((prop, key) => {
+              return (
+                <li
+                  className={
+                    this.activeRoute(prop.path) +
+                    (prop.pro ? " active-pro" : "")
+                  }
+                  key={key}
+                >
+                  <NavLink
+                    to={prop.layout + prop.path}
+                    className="nav-link"
+                    activeClassName="active"
+                  >
+                    <i className={prop.icon} />
+                    <p>{prop.name}</p>
+                  </NavLink>
+                </li>
+              );
+            })}
+          </Nav>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Sidebar;
