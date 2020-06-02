602012a26a135a58ab85febf90c1838e6ba4d9f3
Aniket Kumar <aniketkumar049@gmail.com>
2020-06-02 13:13:12 +0530

    table production and downtime addded

--- src/components/Tabs/ProductionTab.jsx
+++ src/components/Tabs/ProductionTab.jsx
@@ -14,6 +14,7 @@ const ProductionTab = () => {
       "Total Rejection",
       "Status",
       "Edit",
+      "Approve",
     ],
     tableData: [
       [
--- src/variables/tablelist.jsx
+++ src/variables/tablelist.jsx
@@ -1,11 +1,35 @@
 import React, { Component } from "react";
 import { Table } from "reactstrap";
-
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 class TableList extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      checkedColor: "black",
+      uncheckedColor: "black",
+    };
   }
+
+  onCheckedHandler = () => {
+    this.setState((prevState) => {
+      if (prevState.checkedColor === "black") {
+        return (prevState.checkedColor = "#28a745");
+      } else {
+        return (prevState.checkedColor = "black");
+      }
+    });
+  };
+
+  onRejectHandler = () => {
+    this.setState((prevState) => {
+      if (prevState.uncheckedColor === "black") {
+        return (prevState.uncheckedColor = "#dc3545");
+      } else {
+        return (prevState.uncheckedColor = "black");
+      }
+    });
+  };
+
   render() {
     const { tableHead, tableData } = this.props;
     return (
@@ -40,7 +64,8 @@ class TableList extends Component {
                   )
                 )}
 
-                {tableHead[tableHead.length - 1] === "Edit" ? (
+                {tableHead[tableHead.length - 2] === "Edit" ||
+                tableHead[tableHead.length - 1] === "Edit" ? (
                   <td key={tableData.indexOf(item)}>
                     <img
                       alt=""
@@ -48,6 +73,35 @@ class TableList extends Component {
                       onClick={this.props.toggle}
                       src={require("../assets/img/edit-square.svg")}
                     />
+                  </td>
+                ) : null}
+                {tableHead[tableHead.length - 1] === "Approve" ? (
+                  <td key={tableData.indexOf(item)}>
+                    {/* <img
+                      alt=""
+                      className="pointer-cursor"
+                      onClick={this.props.toggle}
+                      src={require("../assets/img/edit-square.svg")}
+                    /> */}
+                    <div
+                      style={{
+                        display: "flex",
+                        justifyContent: "space-around",
+                      }}
+                    >
+                      <FontAwesomeIcon
+                        icon={["far", "check-circle"]}
+                        size="lg"
+                        color={this.state.checkedColor}
+                        onClick={this.onCheckedHandler}
+                      />
+                      <FontAwesomeIcon
+                        icon={["far", "times-circle"]}
+                        size="lg"
+                        color={this.state.uncheckedColor}
+                        onClick={this.onRejectHandler}
+                      />
+                    </div>
                   </td>
                 ) : null}
               </tr>
